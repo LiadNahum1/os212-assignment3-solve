46fe6cbd3d467e27b23b59c468617c01aa889268
LiadNahum1 <liadna@post.bgu.ac.il>
2021-05-21 20:24:22 +0300

    with patch file

--- kernel/vm.c
+++ kernel/vm.c
@@ -88,8 +88,8 @@ kvminithart()
 pte_t *
 walk(pagetable_t pagetable, uint64 va, int alloc)
 {
-  //if(va >= MAXVA)
-    //panic("walk");
+  if(va >= MAXVA)
+    panic("walk");
 
   for(int level = 2; level > 0; level--) {
     pte_t *pte = &pagetable[PX(level, va)];
